f2a6740bd4cc2c1da295d18015e16bedf01aa587
vauxhall <zero.griffin@gmail.com>
2020-04-19 20:13:31 +0200

    Update wheel

--- Shared/View.swift
+++ Shared/View.swift
@@ -22,7 +22,28 @@ final class View: SKView, SKSceneDelegate {
         self.time = time
     }
     
+    override func mouseDown(with: NSEvent) {
+        print(convert(with))
+        if valid(convert(with)) {
+//            drag = .start(x: 0, y: 0)
+            print("valid")
+        } else {
+            print("not")
+        }
+    }
+    
     override func mouseUp(with: NSEvent) {
         (state.currentState as! GKState.State).press.insert(with.location(in: scene!), at: 0)
+    }
+    
+    private func convert(_ event: NSEvent) -> CGPoint {
+        {
+            .init(x: $0.x - frame.midX, y: $0.y - frame.midY + 150)
+        } (convert(event.locationInWindow, from: nil))
+    }
+    
+    private func valid(_ point: CGPoint) -> Bool {
+        let distance = pow(point.x, 2) + pow(point.y, 2)
+        return distance > 900 && distance < 19_600
     }
 }
