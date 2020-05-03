6cc95585d49bfe43b679fb0ba3dbcf1a37fc0a87
vauxhall <zero.griffin@gmail.com>
2020-05-03 11:57:36 +0200

    Update settings

---
+++ mac/Separator.swift
@@ -0,0 +1,14 @@
+import AppKit
+
+final class Separator: NSView {
+    required init?(coder: NSCoder) { nil }
+    init() {
+        super.init(frame: .zero)
+        translatesAutoresizingMaskIntoConstraints = false
+        wantsLayer = true
+    }
+    
+    override func updateLayer() {
+        layer!.backgroundColor = NSColor.separatorColor.cgColor
+    }
+}
--- mac/Settings.swift
+++ mac/Settings.swift
@@ -11,18 +11,60 @@ final class Settings: NSView {
         let scroll = Scroll()
         addSubview(scroll)
         
+        let separator = Separator()
+        addSubview(separator)
+        
         let done = Button(.key("Done"))
         done.target = self
         done.action = #selector(self.done)
         addSubview(done)
         
+        let title = Label(.key("Choose.your.skin"), .regular(16))
+        scroll.add(title)
+        
+        scroll.topAnchor.constraint(equalTo: topAnchor, constant: 1).isActive = true
+        scroll.leftAnchor.constraint(equalTo: leftAnchor, constant: 1).isActive = true
+        scroll.rightAnchor.constraint(equalTo: rightAnchor, constant: -1).isActive = true
+        scroll.bottomAnchor.constraint(equalTo: separator.topAnchor).isActive = true
+        scroll.width.constraint(equalTo: scroll.widthAnchor).isActive = true
+        scroll.height.constraint(greaterThanOrEqualTo: scroll.heightAnchor).isActive = true
+        
+        separator.leftAnchor.constraint(equalTo: leftAnchor, constant: 1).isActive = true
+        separator.rightAnchor.constraint(equalTo: rightAnchor, constant: -1).isActive = true
+        separator.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        separator.bottomAnchor.constraint(equalTo: done.topAnchor, constant: -10).isActive = true
+        
         done.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-        done.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20).isActive = true
+        done.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10).isActive = true
+        
+        title.topAnchor.constraint(equalTo: scroll.top, constant: 50).isActive = true
+        title.leftAnchor.constraint(equalTo: scroll.left, constant: 20).isActive = true
     }
     
     @objc private func done() {
         guard active else { return }
         active = false
-        window?.show(Menu())
+        window!.show(Menu())
+    }
+}
+
+private final class Item: Control {
+    let id: Skin.Id
+    
+    required init?(coder: NSCoder) { nil }
+    init(id: Skin.Id) {
+        self.id = id
+        super.init()
+        let skin = Skin.make(id: id)
+        
+        let image = NSImageView(image: NSImage(named: skin.texture)!)
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.imageScaling = .scaleNone
+        addSubview(image)
+        
+        image.widthAnchor.constraint(equalToConstant: 32).isActive = true
+        image.heightAnchor.constraint(equalToConstant: 32).isActive = true
+        image.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        image.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
     }
 }
